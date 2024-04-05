bf0f2f7b4ba09d50c8a400fd82d11b0d1ae50977
hancom02 <20520484@gm.uit.edu.vn>
2024-04-05 09:16:24 +0700

    Change_Password

--- app/src/main/java/com/example/mobileappdev_nt118n11/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/mobileappdev_nt118n11/ui/profile/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.mobileappdev_nt118n11.ui.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,10 +9,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mobileappdev_nt118n11.Model.User;
 import com.example.mobileappdev_nt118n11.R;
+import com.example.mobileappdev_nt118n11.SignIn;
 import com.example.mobileappdev_nt118n11.databinding.FragmentHomeBinding;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -24,23 +29,26 @@ public class ProfileFragment extends Fragment {
     private FragmentHomeBinding binding;
     private String key;
     private FirebaseDatabase database;
-    private TextView tvName, tvPhone, tvEmail, tvAddress;
-
+    private TextView tvPhone;
+    private EditText edName, edEmail,edAddress;
+    private Button btnUpdate, btnChangePass;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
         init(root);
 
-        key = "20520137";
+
+        key = Phone.Key_Phone;
         database = FirebaseDatabase.getInstance();
         database.getReference().child("User").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 User user = snapshot.child(key).getValue(User.class);
-                tvName.setText(user.getName());
-                tvEmail.setText(user.getGmail());
-                tvAddress.setText(user.getAddress());
+                tvPhone.setText(key);
+                edName.setText(user.getName());
+                edEmail.setText(user.getGmail());
+                edAddress.setText(user.getAddress());
             }
 
             @Override
@@ -48,13 +56,32 @@ public class ProfileFragment extends Fragment {
 
             }
         });
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                database.getReference().child("User").child(key).child("Name").setValue(edName.getText().toString());
+                database.getReference().child("User").child(key).child("Address").setValue(edAddress.getText().toString());
+                database.getReference().child("User").child(key).child("Gmail").setValue(edEmail.getText().toString());
+
+            }
+        });
+        btnChangePass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ChangePass = new Intent(getActivity(), ChangePasswordActivity.class);
+                startActivity(ChangePass);
+                getActivity().finish();
+            }
+        });
         return root;
     }
 
     public void init(View root) {
-        tvName = root.findViewById(R.id.tv_profile_name);
+        edName = root.findViewById(R.id.tv_profile_name);
         tvPhone = root.findViewById(R.id.tv_profile_phone);
-        tvEmail = root.findViewById(R.id.tv_profile_email);
-        tvAddress = root.findViewById(R.id.tv_profile_address);
+        edEmail = root.findViewById(R.id.tv_profile_email);
+        edAddress = root.findViewById(R.id.tv_profile_address);
+        btnUpdate =root.findViewById(R.id.btn_profile_Edit);
+        btnChangePass=root.findViewById(R.id.btn_profile_ChangePass);
     }
 }
