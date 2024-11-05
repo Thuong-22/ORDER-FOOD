de81e139d0b3d4d5e82e12be0e7ccf2b988fd255
keylynk <20521549@gm.uit.edu.vn>
2024-11-05 18:23:41 +0700

    Admin add and edit type

--- app/src/main/java/com/example/mobileappdev_nt118n11/ui/profile/Phone.java
+++ app/src/main/java/com/example/mobileappdev_nt118n11/ui/profile/Phone.java
@@ -1,17 +1,5 @@
 package com.example.mobileappdev_nt118n11.ui.profile;
 
 public class Phone {
-    public static String Key_Phone;
-
-    public Phone() {
-
-    }
-
-    public static String getKey_Phone() {
-        return Key_Phone;
-    }
-
-    public static void setKey_Phone(String key_Phone) {
-        Key_Phone = key_Phone;
-    }
+    public static String Key_Phone="";
 }
